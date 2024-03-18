471149fdfd726dd5962913a9b83fe536cbceb686
fasunareuu <yt.fasu@bk.ru>
2024-03-18 12:00:45 +0300

    task3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { program } from "commander"
+import { fs } from 'file-system'
+import path from 'path'
+import * as parser from '../bin/parser.js'
+import { readFileSync } from 'node:fs';
 
 program
   .name('gendiff')
@@ -7,5 +11,10 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-
+  .action((path1, path2) => {
+    const file1 = fs.readFileSync(parser.resolvePath(path1), { encoding: 'utf-8' })
+    const file2 = fs.readFileSync(parser.resolvePath(path2), { encoding: 'utf-8' })
+    console.log(file1);
+    console.log(file2);
+  })
 program.parse()
