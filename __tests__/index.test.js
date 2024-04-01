9c3b404a280f461f617ea406e50d532516ceb7f5
fasunareuu <yt.fasu@bk.ru>
2024-04-01 18:37:10 +0300

    HELP SOMEONE

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,21 +9,21 @@ const plainResult = readFileSync('__fixtures__/expected.plain.txt', 'utf-8');
 const jsonResult = readFileSync('__fixtures__/expected.json.txt', 'utf-8');
 
 test('testing stylish nested', () => {
-  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(stylishResult);
-  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toBe(stylishResult);
-  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toBe(stylishResult);
+  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(stylishResult);
+  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(stylishResult);
+  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(stylishResult);
 });
 
 test('testing plain nested', () => {
-  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toBe(plainResult);
-  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain')).toBe(plainResult);
-  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toBe(plainResult);
+  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json', 'plain')).toEqual(plainResult);
+  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'plain')).toEqual(plainResult);
+  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toEqual(plainResult);
 });
 
 test('testing json nested', () => {
-  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toBe(jsonResult);
-  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json')).toBe(jsonResult);
-  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toBe(jsonResult);
+  expect(diffEngine('__fixtures__/file1.json', '__fixtures__/file2.json', 'json')).toEqual(jsonResult);
+  expect(diffEngine('__fixtures__/file1.yaml', '__fixtures__/file2.yaml', 'json')).toEqual(jsonResult);
+  expect(diffEngine('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toEqual(jsonResult);
 });
 
 test('should be errors', () => {
